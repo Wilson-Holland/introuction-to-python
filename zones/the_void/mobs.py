bcd753931365e0a00ab342db716b29c912758b4c
asherlegutki-create <asher.legutki@gmail.com>
2026-05-11 18:46:16 +0000

    h

--- zones/the_void/mobs.py
+++ zones/the_void/mobs.py
@@ -115,26 +115,24 @@ TEMPLATES: dict[str, dict] = {
         "name": "weasel",
         "key_words": ("weasel"),
         "room_description": "&wA Weasel floats here randomly.&N",
-<<<<<<< HEAD
-        "description": ("
+        "description": (
             "It is very soft and chubby.\n",
             "It seems like it standing on some cheese."
-<<<<<<< HEAD
+
         ),
         "race":     "animal",
         "class":    "weasel",
         "level":    50,
         "stats":    [60, 100, 60, 100, 75, 100],
-=======
+
         },
         "race":     "non_human",
         "class":    "Weasel",
         "level":    50,
         "stats":    [100, 100, 100, 90, 90, 100],
->>>>>>> 35107d649d4e6c4d4fdc2ab53e3dc231b54d4907
         "aggro":    False,
         "wander":   False,
-=======
+
         "description": (
             "It is very soft and chubby.\n",
             "It seems like it standing on some cheese.",
@@ -145,7 +143,6 @@ TEMPLATES: dict[str, dict] = {
         "stats": [100, 100, 100, 90, 90, 100],
         "aggro": False,
         "wander": False,
->>>>>>> 34fcd46834ed34095f4816862f083b903b8e0d31
     },
     "Nessie": {
         "name": "Nessie",
